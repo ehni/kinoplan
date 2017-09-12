95f6439c056a211698c49f32e938390dd89d9d77
Ehni <rehni@outlook.com>
2017-09-12 23:00:05 +0200

    http -> https for getJSON

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
     console.log("Today:\t" + todayFormatted);
 
-    $.getJSON("http://whateverorigin.org/get?url=" +
+    $.getJSON("https://whateverorigin.org/get?url=" +
         encodeURIComponent("http://www.citydome-sinsheim.com/programm") + "&callback=?",
         function (data) {
             //console.log("> ", data);
